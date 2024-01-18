71f39dd5d2c3419c755e8a4bfacccdf0c28bb728
nedkonedkov001 <nedkonedkov001@gmail.com>
2024-01-18 23:39:48 +0100

    Start from scratch

--- CarParkingAssistant/CarParkingAssistant.Infrastructure/Models/Worker.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace CarParkingAssistant.Infrastructure.Models
-{
-    public class Worker : CarParkingAssistantUser
-    {
-        public required string EGN { get; set; }
-        public required virtual ICollection<Parking> AssignedParkings { get; set; }
-        public required float Salary { get; set; }
-    }
-}
