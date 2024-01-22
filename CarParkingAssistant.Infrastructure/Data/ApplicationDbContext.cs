eaac8eb12ffa85e7f470a92a1113e19640ba6a50
nedkonedkov001 <nedkonedkov001@gmail.com>
2024-01-22 20:21:20 +0100

    Configured database context

--- CarParkingAssistant.Infrastructure/Data/ApplicationDbContext.cs
+++ CarParkingAssistant.Infrastructure/Data/ApplicationDbContext.cs
@@ -1,13 +1,25 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using CarParkingAssistant.Infrastructure.Data.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace CarParkingAssistant.Infrastructure.Data
 {
-    public class ApplicationDbContext : IdentityDbContext
+    public class ApplicationDbContext : IdentityDbContext<CarParkingAssistantUser>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
+
         }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+
+            base.OnModelCreating(builder);
+        }
+
+        public DbSet<Booking> Bookings { get; set; }
+        public DbSet<Parking> Parkings{ get; set; }
+        public DbSet<Worker> Workers { get; set; }
     }
 }
