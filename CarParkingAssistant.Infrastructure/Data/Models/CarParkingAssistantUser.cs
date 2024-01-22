d45e176a50199476bcd4371870f1ca55147336b4
nedkonedkov001 <nedkonedkov001@gmail.com>
2024-01-22 16:46:26 +0100

    Added custom user class

--- CarParkingAssistant.Infrastructure/Data/Models/CarParkingAssistantUser.cs
+++ CarParkingAssistant.Infrastructure/Data/Models/CarParkingAssistantUser.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,5 +10,19 @@ namespace CarParkingAssistant.Infrastructure.Data.Models
 {
     public class CarParkingAssistantUser : IdentityUser
     {
+        [Required]
+        [StringLength(25, ErrorMessage = "The {0} must be between {2} and {1} characters long.", MinimumLength = 2)]
+        public string? FirstName { get; set; }
+
+        [Required]
+        [StringLength(25, ErrorMessage = "The {0} must be between {2} and {1} characters long.", MinimumLength = 2)]
+        public string? LastName { get; set; }
+
+
+        [Required]
+        public DateTime RegisterDate { get; set; }
+
+        [Required]
+        public bool IsDeleted { get; set; }
     }
 }
