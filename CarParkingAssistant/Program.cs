4a350473663b5579c357141b3bc7488b4b6283dc
nedkonedkov001 <nedkonedkov001@gmail.com>
2024-01-28 17:04:24 +0100

    Mapped admin area

--- CarParkingAssistant/Program.cs
+++ CarParkingAssistant/Program.cs
@@ -57,6 +57,10 @@ app.UseAuthorization();
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
+app.
+    MapControllerRoute(
+    name: "Admin",
+    pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
 app.MapRazorPages();
 
 app.Run();
