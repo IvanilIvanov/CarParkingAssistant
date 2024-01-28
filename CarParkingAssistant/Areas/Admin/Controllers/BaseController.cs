869db5eef05dc9604c0fca238f9f1f5397d4e99b
nedkonedkov001 <nedkonedkov001@gmail.com>
2024-01-28 16:58:02 +0100

    Added admin area

---
+++ CarParkingAssistant/Areas/Admin/Controllers/BaseController.cs
@@ -0,0 +1,12 @@
+﻿using CarParkingAssistant.Core.Constants;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CarParkingAssistant.Areas.Admin.Controllers
+{
+    [Authorize(Roles = UserConstants.Roles.Admin)]
+    [Area("Admin")]
+    public class BaseController : Controller
+    {
+    }
+}
