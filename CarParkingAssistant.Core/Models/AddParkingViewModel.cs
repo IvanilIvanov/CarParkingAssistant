4f90ab068096ed0a9854469d5e3ed79a00267bbf
IvanilIvanov <ivanilivanov1@gmail.com>
2024-02-03 13:48:13 +0200

    Added parking add page

---
+++ CarParkingAssistant.Core/Models/AddParkingViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarParkingAssistant.Core.Models
+{
+    public class AddParkingViewModel
+    {
+        public string Address { get; set; }
+        public int NumSlots { get; set; }
+        public string Name { get; set; }
+    }
+}
