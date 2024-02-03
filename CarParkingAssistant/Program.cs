1d0434e9fca356906be8fe06a72f9aee6f67eaf0
IvanilIvanov <ivanilivanov1@gmail.com>
2024-02-03 13:48:50 +0200

    Changed MapRoute area

--- CarParkingAssistant/Program.cs
+++ CarParkingAssistant/Program.cs
@@ -54,13 +54,15 @@ app.UseRouting();
 app.UseAuthentication();
 app.UseAuthorization();
 
+app.
+    MapControllerRoute(
+    name: "area",
+    pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
+
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
-app.
-    MapControllerRoute(
-    name: "Admin",
-    pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
+
 app.MapRazorPages();
 
 app.Run();
